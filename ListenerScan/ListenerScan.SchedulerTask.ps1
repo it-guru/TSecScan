c7e730f682f4d7bc9488dece2f2ab547cea141f5
TSecScan <hartmut.vogler@t-systems.com>
2019-06-25 13:17:15 +0200

    add Threaded processing for ListenerScan

--- ListenerScan/ListenerScan.SchedulerTask.ps1
+++ ListenerScan/ListenerScan.SchedulerTask.ps1
@@ -1,13 +1,16 @@
 param(
    [string]$Config     = "\etc\SchedulerTask.ini",
-   [string]$LocalAdminUser          = "nobody",
-   [string]$LocalAdminUserPassword  = "nopass"
+   [string]$LogDir     = "\tmp",
+   [string]$LocalAdminUser       = "nobody",
+   [string]$LocalAdminUserPassword   = "nopass"
 )
 
-$MyDir  =[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
-$MyApp  =[System.IO.Path]::GetFileName($MyInvocation.MyCommand.Name)
-$MyDrive=Split-Path -Path $MyDir -Qualifier
-$MyName =Split-Path -Path $MyApp -Leaf
+$MyDir   =[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+$MyApp   =[System.IO.Path]::GetFileName($MyInvocation.MyCommand.Name)
+$MyDrive =Split-Path -Path $MyDir -Qualifier
+$MyName  =Split-Path -Path $MyApp -Leaf
+$MyCampania=Split-Path -Path $MyDir -Leaf
+
 Set-Location $MyDrive
 $env:PSModulePath = $env:PSModulePath + ';'+$MyDir+'\..\PowerLib';
 Import-Module -Name ScanKernel
@@ -16,30 +19,37 @@ Write-Log "Start $MyName";
 
 Write-Log "Path        : '$MyDir'";
 Write-Log "Drive       : '$MyDrive'";
+Write-Log "Campania    : '$MyCampania'";
 
 Import-Module -Name Recon
 Import-Module -Name ConfigFile
 
 Import-ConfigFile -Ini -ErrorAction Stop -ConfigFilePath $Config
 
-Write-Log "User        : '$LocalAdminUser'";
+$MyJobname=$MyName -replace "\.SchedulerTask.ps1$","";
 
-$taskName="TSecScan-ListenerScan"
+Write-Log "Jobname     : '$MyJobname'";
+Write-Log "User        : '$User'";
+Write-Log "LogDir      : '$LogDir'";
+
+$taskName="TSecScan-$MyCampania-$MyJobname";
 
 $taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName }
 
 if ($taskExists){
    Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
 }
-$repeat = (New-TimeSpan -Hours 4)
-$maxduration = (New-TimeSpan -Hours 4)
+$maxduration = (New-TimeSpan -Hours 6)
 
-$cmd='-ExecutionPolicy Bypass "& ''d:\Program Files\TSecScan\ListenerScan\ListenerScan.ps1'' -NoProfile -Config \etc\ListenerScan.ini >>D:\data\w5secscan\log\ListenerScan.log 2>&1'
+$cmd="-ExecutionPolicy Bypass -NoProfile ""& "+ `
+     "'$MyDir\$MyJobname.ps1' "+ `
+     " -Config \etc\$MyCampania.ini "+ `
+     ">>$LogDir\$MyCampania-$MyJobname.log 2>&1 "" ";
 
-$TaskTrigger = New-ScheduledTaskTrigger -At 19:10 -Daily;
+$TaskTrigger = New-ScheduledTaskTrigger -At 20:40 -Daily;
 $TaskAction  = New-ScheduledTaskAction -Execute powershell.exe `
                                        -Argument $cmd `
-                                       -WorkingDirectory 'd:\'; 
+                                       -WorkingDirectory "$MyDir\"; 
 $TaskSettings= New-ScheduledTaskSettingsSet -ExecutionTimeLimit $maxduration;
 
 
@@ -50,8 +60,7 @@ $nTask=Register-ScheduledTask -TaskName $TaskName `
                        -Action $TaskAction;
 
 $nTask.Triggers.Repetition.Duration="P1D";
-$nTask.Triggers.Repetition.Interval ="PT4H";
+$nTask.Triggers.Repetition.Interval ="PT12H";
 
-$n=$nTask | Set-ScheduledTask -Password $LocalAdminUserPassword `
-                              -User $LocalAdminUser
+$n=$nTask | Set-ScheduledTask -Password $LocalAdminUserPassword -User $LocalAdminUser
 
--- ListenerScan/ListenerScan.ps1
+++ ListenerScan/ListenerScan.ps1
@@ -5,7 +5,8 @@ param(
    [string]$DB         = "\tmp",
    [string]$AddPath    = $null, 
    [string]$ExportDir  = "\tmp",
-   [long]$MaxWorkTime = 3600
+   [long]$Threads      = 10,
+   [long]$MaxWorkTime  = 14400
 )
 
 $MyDir  =[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
@@ -43,6 +44,7 @@ Write-Log "AddPath       : '$AddPath'"
 
 # local script variables 
 Write-Log "Ports         : '$Ports'"
+Write-Log "Threads       : '$Threads'"
 Write-Log "MaxWorkTime   : '$MaxWorkTime'"
 Write-Log ""
 
@@ -50,7 +52,10 @@ $StartDate=Get-Date;
 $TimedOut=$false;
 
 
-
+$ScriptParams = @{
+    'Ports' = $Ports;
+    'Drive' = $myDrive
+};
 
 
 $NetworksFile="$ControlDir/Networks.csv";
@@ -69,71 +74,118 @@ if ( -not (Test-Path "$DB\Network")){
 }
 
 
-
-
-
-$NetList=Get-Content $NetworksFile;
-
-for($fnum=1;$fnum -le 9999; $fnum++){
-   $OutFile="$DB\Network\Nodes_{0:d4}" -f $fnum;
-   $now=Get-Date;
-   if ((New-TimeSpan -Start $StartDate -End $now).TotalSeconds -gt $MaxWorkTime){
-      $TimedOut=$true;
-   }
-   else{
-      if ($NetList[$fnum-1]){
-         $csvline=$NetList[$fnum-1].split(";");
-         if ( -not (Test-Path "$OutFile.csv1")){
-            $networkspec=$csvline[0];
-            Write-Log "start Processing $($networkspec) to $OutFile.csv1";
-            Write-Output $networkspec | foreach-object {
-               Write-Output $_ | & 'nmap' --min-rtt-timeout 2000ms `
-                                          --initial-rtt-timeout 2000ms `
-                                          --min-parallelism 1024 `
-                                          --host-timeout 60s `
-                                          -Pn `
-                                          -v -oG - -iL -  `
-                                          -p $Ports 2> $null
-            } | foreach-object {
-              if (-not ($_ -match "Status: Down")){
-                 if ($_ -match "Ports:"){
-                    $l=[regex]::split($_,"(\s*Host:\s*|\s*Ports:\s*)") | `
-                                                           foreach-object {
-                       $_.trim();
-                    };
-                    $rec = new-object System.Collections.Hashtable
-                    for($i=1;$i -lt $l.Length;$i+=2) {
-                       $k=$l[$i] -replace "[^a-z0-9A-Z]","";
-                       $rec.Add($k,$l[$i+1]);
-                    }
-                    if ($rec.Host -match "[()]"){
-                       $rec.DnsName=[regex]::replace($rec.Host, `
-                                                     "^.* \((.+)\).*$",'$1');
-                       $rec.Host=$rec.Host -replace " .*$","";
-                    }
-                    Write-Output "$($rec.Host);$($rec.DnsName);$($rec.Ports)";
-                 }
-              }
-            } > "$OutFile.tmp"
-            Move-Item -Force -Path "$OutFile.tmp" -Destination "$OutFile.csv1"
-            Copy-Item -Force -Path "$OutFile.csv1" -Destination "$OutFile.csv"
+$networkScanBlock= {
+    Param (
+        [Object]
+        $ComputerName=$null,
+        [String]
+        $Drive=$null,
+        [String]
+        $Ports=$null
+    )
+    Set-Location $Drive;
+    $networkspec=$ComputerName.networkspec;
+    $OutFile=$ComputerName.OutFile;
+    $tmpfile="$OutFile.tmp";
+    Write-Log "start Processing $($networkspec) to $OutFile.csv1";
+    Write-Output $networkspec | foreach-object {
+       Write-Output $_ | & 'nmap' --min-rtt-timeout 2000ms `
+                                  --initial-rtt-timeout 2000ms `
+                                  --min-parallelism 100 `
+                                 --host-timeout 90s `
+                                  -Pn `
+                                  -v -oG - -iL -  `
+                                  -p $Ports 2>&1
+    } | foreach-object {
+      #Write-Host $_;
+      if (-not ($_ -match "Status: Down")){
+         if (($_ -match "Ports:") -and ($_ -match "/open/")){
+            $l=[regex]::split($_,"(\s*Host:\s*|\s*Ports:\s*)") | `
+                                                   foreach-object {
+               $_.trim();
+            };
+            $rec = new-object System.Collections.Hashtable
+            for($i=1;$i -lt $l.Length;$i+=2) {
+               $k=$l[$i] -replace "[^a-z0-9A-Z]","";
+               $rec.Add($k,$l[$i+1]);
+            }
+            if ($rec.Host -match "[()]"){
+               $rec.DnsName=[regex]::replace($rec.Host, `
+                                             "^.* \((.+)\).*$",'$1');
+               $rec.Host=$rec.Host -replace " .*$","";
+            }
+            Write-Output "$($rec.Host);$($rec.DnsName);$($rec.Ports)";
          }
       }
-      else{
-         Get-Item "$OutFile.*" | Remove-Item
-      }
-   }
-}
-if (-not ($TimedOut)){
-   Write-Host "cleanup csv1 files"
-   Get-Item "$DB\Network\Nodes_*.csv1" | Remove-Item -Force
+    } > $tmpfile
+    Write-Log "end Processing $($networkspec) to $OutFile.csv1";
+    Move-Item -Force -Path "$OutFile.tmp" -Destination "$OutFile.csv1"
+    Copy-Item -Force -Path "$OutFile.csv1" -Destination "$OutFile.csv"
 }
 
-Get-Item "$ExportDir\NetworkNodes_*.csv" | Remove-Item -Force
-$d=Get-Date -Format "yyyyMMdd-HHmmss";
-$OutFile="$ExportDir\NetworkNodes_$d.csv";
-Set-Content $OutFile -Value "Host;DNSName;Ports";
-Get-Content "$DB\Network\Nodes_*.csv" | Add-Content $OutFile
+
+
+
+try{
+   $NetList=Get-Content $NetworksFile;
+
+   $scanbuffer=@();
+   for($fnum=1;$fnum -le 9999; $fnum++){
+      $OutFile="$DB\Network\Nodes_{0:d4}" -f $fnum;
+      $now=Get-Date;
+      if ((New-TimeSpan -Start $StartDate -End $now).TotalSeconds `
+          -gt $MaxWorkTime){
+         $TimedOut=$true;
+      }
+      else{
+         if ($NetList[$fnum-1]){
+            $csvline=$NetList[$fnum-1].split(";");
+            if ( (-not (Test-Path "$OutFile.csv1")) ){
+               $rec=@{
+                  networkspec=$csvline[0];
+                  OutFile=$OutFile;
+               };
+               $scanbuffer+=$rec;
+            }
+         }
+         else{
+            Get-Item "$OutFile.*" | Remove-Item
+         }
+         if ($scanbuffer.Count -gt $Threads-1){
+            Invoke-XxXThreadedFunction -Verbose -ComputerName $scanbuffer `
+                                       -ScriptParameters $ScriptParams `
+                                       -ScriptBlock $networkScanBlock `
+                                       -Threads $Threads | Foreach-Object {
+               Write-Output $_;
+            }
+            $scanbuffer=@();
+         }
+      }
+   }
+   if ($scanbuffer.Count -gt 0){
+      Invoke-XxXThreadedFunction -Verbose -ComputerName $scanbuffer `
+                                 -ScriptParameters $ScriptParams `
+                                 -ScriptBlock $networkScanBlock `
+                                 -Threads $Threads | Foreach-Object {
+         Write-Output $_;
+      }
+      $scanbuffer=@();
+   }
+   if (-not ($TimedOut)){
+      Write-Host "cleanup csv1 files"
+      Get-Item "$DB\Network\Nodes_*.csv1" | Remove-Item -Force
+   }
+
+   Get-Item "$ExportDir\NetworkNodes_*.csv" | Remove-Item -Force
+   $d=Get-Date -Format "yyyyMMdd-HHmmss";
+   $OutFile="$ExportDir\NetworkNodes_$d.csv";
+   Set-Content $OutFile -Value "Host;DNSName;Ports";
+   Get-Content "$DB\Network\Nodes_*.csv" | Add-Content $OutFile
+}
+finally{
+  Write-Host "Exit"
+  Get-Item "$DB\Network\Nodes_*.tmp" | Remove-Item -Force
+}
 
 
 
