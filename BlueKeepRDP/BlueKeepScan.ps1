f9e1e2746fcc71152043cc1de5d4cf774267237a
TSecScan <hartmut.vogler@t-systems.com>
2019-07-26 16:30:37 +0200

    transfer rdpscan in single threads with timeout of 120 sec

--- BlueKeepRDP/BlueKeepScan.ps1
+++ BlueKeepRDP/BlueKeepScan.ps1
@@ -4,7 +4,7 @@ param(
    [string]$DB         = "\tmp",
    [string]$AddPath    = $null, 
    [string]$ExportDir  = "\tmp",
-   [long]$MaxWorkTime = 3600
+   [long]$MaxWorkTime = 7200
 )
 
 $MyDir  =[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
@@ -63,6 +63,41 @@ if ( -not (Test-Path "$DB\BlueKeep")){
 }
 
 
+$rdpscanBlock= {
+    Param (
+        [String]
+        $ComputerName=$null,
+        [String]
+        $Drive=$null,
+        [String]
+        $Ports=$null
+    )
+    #Write-Host "Start Scan on $ComputerName"
+
+    & 'rdpscan' "$ComputerName/32" |  foreach-object {
+        $l=[regex]::split($_," - ");
+        $treatRules="IgnoreFinding";
+        if ($l[1] -match "VULNERABLE"){
+           $treatRules="EnforceRemove";
+        }
+        if ($True){ #$l[1] -match "VULNERABLE"){
+           $token="BlueKeep:$($l[0]):RDP";
+           $ipaddr=$l[0];
+           if (-not ($ipaddr -eq "")){
+              $d=(Get-Date).ToUniversalTime().ToString(`
+                 "yyyy-MM-dd HH:mm:ss");
+              Write-Output ("$token;$d;"+ `
+                            "$ipaddr;$treatRules;BLUEKEEP001;"+ `
+                            "BlueKeep");
+           }
+        }
+    }
+}
+
+
+
+
+
 
 for($fnum=1;$fnum -le 9999; $fnum++){
    $InFile="$DB\Network\Nodes_{0:d4}" -f $fnum;
@@ -74,38 +109,38 @@ for($fnum=1;$fnum -le 9999; $fnum++){
    }
    else{
       if (-not ($TimedOut)){
-         if ((Test-Path "$InFile.csv") -and -not (Test-Path "$OutFile.csv1")){
-            Write-Log "start Processing to $OutFile.csv1";
-            Get-Content "$InFile.csv" | foreach-object {
-               $csvline=$_.split(";");
-               $pline=$csvline[2];
-               $pline=
-               $pline=[regex]::replace($csvline[2], "^.*:","");
-               $plst=$pline.split(",");
-               if ($plst -match "3389/open"){
-                  Write-Output $csvline[0];
+         if (Test-Path "$InFile.csv"){
+            if (-not (Test-Path "$OutFile.csv1")){
+               Write-Log "start Processing to $OutFile.csv1";
+               Get-Content "$InFile.csv" | foreach-object {
+                  $csvline=$_.split(";");
+                  $pline=$csvline[2];
+                  $pline=
+                  $pline=[regex]::replace($csvline[2], "^.*:","");
+                  $plst=$pline.split(",");
+                  if ($plst -match "3389/open"){
+                     Write-Output $csvline[0];
+                  }
+                } > "$OutFile.in"
+                if ((Get-Item "$OutFile.in").length -gt 0kb){ 
+                    $ipList=Get-Content "$OutFile.in"
+                   Invoke-XxXThreadedFunction -Verbose -ComputerName $ipList `
+                                              -ScriptBlock $rdpscanBlock `
+                                              -Timeout 120 `
+                                              -Threads 100 | Foreach-Object {
+                      Write-Output $_;
+                   }  > "$OutFile.tmp"
                }
-            } | & 'rdpscan' --workers 200 --file - | foreach-object {
-               $l=[regex]::split($_," - ");
-               $treatRules="IgnoreFinding";
-               if ($l[1] -match "VULNERABLE"){
-                  $treatRules="EnforceRemove";
+               else{
+                  set-content -Path ("$OutFile.tmp") -Force -Value ($null);
                }
-               $token="BlueKeep:$($l[0]):RDP";
-               $ipaddr=$l[0];
-               if (-not ($ipaddr -eq "")){
-                  $d=(Get-Date).ToUniversalTime().ToString(`
-                     "yyyy-MM-dd HH:mm:ss");
-                  Write-Output ("$token;$d;"+ `
-                                "$ipaddr;$treatRules;BLUEKEEP001;"+ `
-                                "BlueKeep");
-               }
-            }  > "$OutFile.tmp"
-            Move-Item -Force -Path "$OutFile.tmp" `
-                             -Destination "$OutFile.csv1"
-            Copy-Item -Force -Path "$OutFile.csv1" `
-                             -Destination "$OutFile.csv"
-            Write-Log "finish file $OutFile.csv"
+               Move-Item -Force -Path "$OutFile.tmp" `
+                                -Destination "$OutFile.csv1"
+               Copy-Item -Force -Path "$OutFile.csv1" `
+                                -Destination "$OutFile.csv"
+               #Get-Item "$OutFile.in" | Remove-Item -Force
+               Write-Log "finish file $OutFile.csv"
+            }
          }
          else{
             Get-Item "$OutFile.*" | Remove-Item
@@ -113,14 +148,18 @@ for($fnum=1;$fnum -le 9999; $fnum++){
       }
    }
 }
+Write-Log "loop finished with TimedOut=$TimedOut"
 if (-not ($TimedOut)){
-   Write-Host "cleanup csv1 files"
+   Write-Log "cleanup csv1 files"
    Get-Item "$DB\BlueKeep\BlueKeep_*.csv1" | Remove-Item -Force
+   Get-Item "$DB\BlueKeep\BlueKeep_*.in" | Remove-Item -Force
 }
 
+Write-Log "removing old exports $ExportDir\BlueKeepRDP_*.csv"
 Get-Item "$ExportDir\BlueKeepRDP_*.csv" | Remove-Item -Force
 $d=Get-Date -Format "yyyyMMdd-HHmmss";
 $OutFile="$ExportDir\BlueKeepRDP_$d.csv";
+Write-Log "build new $OutFile"
 Set-Content $OutFile -Value ("SecToken;ScanDate;IPAddress;TreatRules" + `
                              ";SecItem;SecCampaign");
 Get-Content "$DB\BlueKeep\BlueKeep_*.csv" | Add-Content $OutFile
